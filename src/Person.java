bac87756e0737f582ddda0b6a4d0e08c970217ab
seksatta (office) <seksattha@gmail.com>
2024-07-20 15:15:42 +0700

    not having a constructor

---
+++ src/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String firstName, lastName, nickname, gender;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
